236d544001ee2e1f0f25254e3eb4ab9c7a77b100
Helsing <ivancool2222@gmail.com>
2024-11-02 17:14:22 +0700

    hudge work

--- rtl/encryption.sv
+++ rtl/encryption.sv
@@ -1,24 +1,30 @@
-module encryption(
+module encryption
+#(
+  parameter round_num = 32, 
+  parameter block_size = 64
+)
+(
   input logic rst,
   input logic clk,
-  input logic [47:0] round_keys [32],
+  input logic [block_size*3/4-1:0] round_keys [round_num],
   
-  input logic [63:0] s_axis_tdata,
+  input logic [block_size-1:0] s_axis_tdata,
   input logic s_axis_tvalid,
   output logic s_axis_tready,
   
   
-  output logic [63:0] m_axis_tdata,
+  output logic [block_size-1:0] m_axis_tdata,
   output logic m_axis_tvalid,
   input logic m_axis_tready
 );
 
-  logic [63:0] block [64];
-  logic valid [32];
-  logic [63:0] encr_block [64];
+  logic [block_size-1:0] block [round_num];
+  logic [block_size-1:0] encr_block [round_num];
+  logic valid [round_num];
+  logic move [round_num];
 
   generate
-    for (genvar i =0; i<32; i++) begin
+    for (genvar i = 0; i < round_num; i++) begin : idk
       round round_inst(
         .idata(block[i]),
         .key(round_keys[i]),
@@ -29,8 +35,8 @@ module encryption(
         if (rst) begin
           valid[i] <= 1'b0;
           block[i] <= '0;
-        end else if (m_axis_tready) begin
-          if (i==0) begin
+        end else if (move[i]) begin
+          if (i == 0) begin
             valid[i] <= s_axis_tvalid;
             block[i] <= s_axis_tdata;
           end else begin
@@ -39,20 +45,28 @@ module encryption(
           end
         end
       end
+      
+      always @(*) begin
+        if (i == round_num - 1) begin
+          if (valid[i] == 0 || m_axis_tready)
+            move[i] <= 1;
+          else
+            move[i] = 0;
+        end else begin
+          if (move [i+1] || valid[i] == 0)
+            move[i] <= 1;
+          else
+            move[i] <= 0;
+        end
+      end
     end
   endgenerate
   
-  always @(posedge clk)begin
-    if (rst) begin
-      m_axis_tdata <= '0;
-      m_axis_tvalid <= 1'b0;
-    end else begin
-      m_axis_tdata <= encr_block[31];
-      m_axis_tvalid <= valid[31];
-    end
-  end
-  
-  //assign s_axis_tready = m_axis_tready;
-  assign s_axis_tready = ~valid[31];
-  
+ 
+    
+  //assign ready[32] = m_axis_tready;
+  assign s_axis_tready = ~valid[round_num - 1];
+  assign m_axis_tdata = encr_block[round_num-1];
+  assign m_axis_tvalid = valid[round_num-1];
+ 
 endmodule
--- rtl/key_setup.sv
+++ rtl/key_setup.sv
@@ -1,23 +1,31 @@
-module key_setup(
+module key_setup
+# (
+  parameter round_num = 32,
+  parameter block_size = 64
+  )
+  (
   input logic rst,
   input logic clk,
-  input logic [127:0] key,
+  input logic [block_size*2-1:0] key,
   
-  input logic [63:0] s_axis_tdata,
+  input logic [block_size-1:0] s_axis_tdata,
   input logic s_axis_tvalid,
+  output logic s_axis_tready,
   
-  output logic [63:0] m_axis_tdata,
+  output logic [block_size-1:0] m_axis_tdata,
+  output logic m_axis_tvalid,
+  input logic m_axis_tready,
 
-  output logic [47:0] round_keys [32],
+  output logic [block_size*3/4-1:0] round_keys [round_num],
   output logic key_ready
   
 );
   
-  parameter [1:0]START = 2'b00, EXP1 = 2'b01, INTER=2'b10, EXP2 = 2'b11;
-  logic [1:0] state = START, next_state;
-  logic [63:0]block;
-  logic [4:0] counter = 5'b0;
-  logic [47:0] K [32];
+  localparam [2:0]START = 3'b000, EXP1 = 3'b001, INTER=3'b010, EXP2 = 3'b011, READY = 3'b100;
+  logic [2:0] state = START, next_state = EXP1; // вероятно проблемы с next_state
+  logic [block_size-1:0]block;
+  logic [$clog2(round_num)-1:0] counter = '0;
+  logic [block_size*3/4-1:0] K [round_num];
   
   logic last;
   assign last = &counter;
@@ -31,8 +39,11 @@ module key_setup(
     case (state) 
       START: next_state = EXP1;
       EXP1: if(last) next_state = INTER;
+        else next_state = EXP1;
       INTER: next_state = EXP2;
-      EXP2: next_state = EXP2;
+      EXP2: if(last) next_state = READY;
+        else next_state = EXP2;
+      READY: next_state = READY;
       default: next_state = START;
     endcase
   end
@@ -42,30 +53,51 @@ module key_setup(
   
   always @(posedge clk) begin
     if (rst) begin
-      counter <= 5'b0;
+      counter <= '0;
       block <= '0;
+      // обнулить раундовый ключ
     end else begin
       case (state)
         START: begin
-          counter <= 5'b0;
-          block <= key[127:64];
+          counter <= '0; // подумать над двойным обнулением
+          block <= key[block_size*2-1:block_size];
+          m_axis_tvalid <= 1;
+          s_axis_tready <= 0;
         end
-        EXP1:
-          if (s_axis_tvalid) begin
-            counter <= counter + 1;
-            block <= s_axis_tdata;
-            K[counter] <= {s_axis_tdata[15:0], s_axis_tdata[63:32]};
+        EXP1: begin
+          if (m_axis_tready && m_axis_tvalid) begin
+            m_axis_tvalid <= 0;
+            s_axis_tready <= 1;
           end
+          if (s_axis_tvalid) begin
+            counter <= counter + '1;
+            block <= s_axis_tdata;
+            m_axis_tvalid <= 1;
+            s_axis_tready <= 0;
+            K[counter] <= {s_axis_tdata[block_size/4-1:0], s_axis_tdata[block_size-1:block_size/2]};
+          end
+        end
         INTER: begin
-          counter <= 5'b0;
-          block <= key[63:0];
+          counter <= '0;
+          block <= key[block_size-1:0];
+          m_axis_tvalid <= 1;
+          s_axis_tready <= 0;
         end
-        EXP2:
-          if (s_axis_tvalid) begin
-            counter <= counter + 1;
-            block <= s_axis_tdata;
-            K[counter] <= K[counter] ^ {s_axis_tdata[15:0], s_axis_tdata[63:32]};
+        EXP2: begin
+          if (m_axis_tready) begin
+            m_axis_tvalid <= 0;
+            s_axis_tready <= 1;
           end
+          if (s_axis_tvalid) begin
+            counter <= counter + '1;
+            block <= s_axis_tdata;
+            K[counter] <= K[counter] ^ {s_axis_tdata[block_size/4-1:0], s_axis_tdata[block_size-1:block_size/2]};
+          end
+        end
+        READY: begin
+          m_axis_tvalid <= 0;
+          s_axis_tready <= 0;
+        end
       endcase
     end
   end
@@ -76,6 +108,6 @@ module key_setup(
           
   assign m_axis_tdata = block;
   assign round_keys = K;
-  assign key_ready = (state == EXP2) & last;
+  assign key_ready = (state == READY);
   
 endmodule
